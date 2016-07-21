0d0d537d5bb5594d028ef452b9b4225a4832b1a9
Larry Shaffer <lshaffer@boundlessgeo.com>
2016-07-20 19:55:09 -0600

    Use docker exec to tar for full backup on running container

--- docker-plugins-xml/repo-full-backup.sh
+++ docker-plugins-xml/repo-full-backup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+set -e
+
 QGIS_BASE=qgisrepo_base_1
-QGIS_CONF=qgisrepo_conf_1
-QGIS_REPO=qgisrepo_data_1
 QGIS_ARCHIVE=qgis-repo-full-backup_$(date +%Y%m%d-%H%M%S).tgz
 ARCHIVE_DIR=$HOME/qgis-repo-backup
 
@@ -11,27 +11,20 @@ mkdir -p $ARCHIVE_DIR
 echo -e "\nApplying environment..."
 . docker-compose.env
 
-echo -e "\nAttempting to pause running services..."
-docker-compose pause
-
-echo -e "\nAttempting to back up ${QGIS_REPO}'s /www/{qgis,qgis-dev} dirs to \n$ARCHIVE_DIR/${QGIS_ARCHIVE}..."
-docker run --rm \
---volumes-from $QGIS_BASE --volumes-from $QGIS_CONF --volumes-from $QGIS_REPO \
--v $ARCHIVE_DIR:/backup debian:jessie \
-  tar --ignore-failed-read -cvzf /backup/$QGIS_ARCHIVE -C / \
-  /home/${SSH_USER} \
-  /etc/nginx \
-  /etc/ssh \
-  /etc/ssl/nginx \
-  /etc/supervisor \
-  /etc/uwsgi \
-  /opt \
-  /usr/lib/nginx \
-  /var/www
+echo -e "\nAttempting to back up ${QGIS_BASE}'s dirs with data to \n$ARCHIVE_DIR/${QGIS_ARCHIVE}..."
+docker exec $QGIS_BASE \
+tar -C / -cf - \
+  home/${SSH_USER} \
+  etc/nginx \
+  etc/ssh \
+  etc/ssl/nginx \
+  etc/supervisor \
+  etc/uwsgi \
+  opt \
+  usr/lib/nginx \
+  /var/www \
+| gzip > $ARCHIVE_DIR/$QGIS_ARCHIVE
 
 if [ ! -f $ARCHIVE_DIR/$QGIS_ARCHIVE ]; then
   echo -e "\n... backup failed"
 fi
-
-echo -e "\nAttempting to unpause running services..."
-docker-compose unpause
