1c313eb09972dcbbb600f108d258b9699d4380f0
Larry Shaffer <lshaffer@boundlessgeo.com>
2018-09-12 08:32:39 -0600

    Update SSL install script for plugins-xml docker; optional on env vars

--- docker-plugins-xml/services-conf/resources/ssl-custom-cert/install.sh
+++ docker-plugins-xml/services-conf/resources/ssl-custom-cert/install.sh
@@ -11,21 +11,27 @@ fi
 
 QGIS_BASE=qgisrepo_base_1
 
-for ext in 'crt' 'key'; do
-  pki=qgisrepo-server.${ext}
-  if [ -f ${HOME}/.${pki} ]; then
-    docker cp -L ${HOME}/.${pki} ${QGIS_BASE}:/etc/ssl/nginx/${pki}
-  fi
-done
+if [ "${SSL_CUSTOM_CERT}" = "1" ]; then
 
-docker exec -i ${QGIS_BASE} bash <<EOF
+  for ext in 'crt' 'key'; do
+    pki=qgisrepo-server.${ext}
+    if [ -f ${HOME}/.${pki} ]; then
+      docker cp -L ${HOME}/.${pki} ${QGIS_BASE}:/etc/ssl/nginx/${pki}
+    fi
+  done
+
+  docker exec -i ${QGIS_BASE} bash <<EOF
 cd /etc/ssl/nginx/
 chown -f root:root qgisrepo-server* || true
 chmod -f 0600 qgisrepo-server* || true
 EOF
 
-# Let's Encrypt
-docker exec -i ${QGIS_BASE} bash <<EOF
+fi
+
+if [ "${SSL_LETSENCRYPT_CERTS}" = "1" ]; then
+
+  # Let's Encrypt
+  docker exec -i ${QGIS_BASE} bash <<EOF
 sed -i \
 -e 's|\(include incl\.d/ssl_cert_dev;\)|#\1|g' \
 -e 's|# *\(include incl\.d/ssl_cert_letsencrypt_dev;\)|\1|g' \
@@ -38,6 +44,8 @@ certbot certonly -a webroot --webroot-path=/var/www/qgis-beta -d ${DOMAIN_TLD_BE
 EOF
 
 
-docker exec -i ${QGIS_BASE} bash <<EOF
+  docker exec -i ${QGIS_BASE} bash <<EOF
 supervisorctl restart nginx
 EOF
+
+fi
