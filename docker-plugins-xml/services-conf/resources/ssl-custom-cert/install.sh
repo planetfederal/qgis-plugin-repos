1274fab0205b38a2c0fb7800d53dc3cb9a871a96
Larry Shaffer <lshaffer@boundlessgeo.com>
2018-09-12 07:54:11 -0600

    Update SSL install script for plugins-xml docker

--- docker-plugins-xml/services-conf/resources/ssl-custom-cert/install.sh
+++ docker-plugins-xml/services-conf/resources/ssl-custom-cert/install.sh
@@ -18,26 +18,26 @@ for ext in 'crt' 'key'; do
   fi
 done
 
-docker exec -I ${QGIS_BASE} bash <<EOF
-  chmod -f 0600 /etc/ssl/nginx/qgisrepo-server*
+docker exec -i ${QGIS_BASE} bash <<EOF
+cd /etc/ssl/nginx/
+chown -f root:root qgisrepo-server* || true
+chmod -f 0600 qgisrepo-server* || true
 EOF
-
 
 # Let's Encrypt
-docker exec -I ${QGIS_BASE} bash <<EOF
-  sed -i \
-  -e 's|(include[[:space:]]incl\.d/ssl_cert_dev;)|#\1|g' \
-  -e 's|#(include[[:space:]]incl\.d/ssl_cert_letsencrypt_dev;)|\1|g' \
-  -e 's|(include[[:space:]]incl\.d/ssl_cert_beta;)|#\1|g' \
-  -e 's|#(include[[:space:]]incl\.d/ssl_cert_letsencrypt_beta;)|\1|g' \
-  /etc/nginx/conf.d/qgis-repo.conf
+docker exec -i ${QGIS_BASE} bash <<EOF
+sed -i \
+-e 's|\(include incl\.d/ssl_cert_dev;\)|#\1|g' \
+-e 's|# *\(include incl\.d/ssl_cert_letsencrypt_dev;\)|\1|g' \
+-e 's|\(include incl\.d/ssl_cert_beta;\)|#\1|g' \
+-e 's|# *\(include incl\.d/ssl_cert_letsencrypt_beta;\)|\1|g' \
+/etc/nginx/conf.d/qgis-repo.conf
 
-  certbot certonly -a webroot --webroot-path=/var/www/qgis-dev -d ${DOMAIN_TLD_DEV}
-  certbot certonly -a webroot --webroot-path=/var/www/qgis-beta -d ${DOMAIN_TLD_BETA}
-
+certbot certonly -a webroot --webroot-path=/var/www/qgis-dev -d ${DOMAIN_TLD_DEV}
+certbot certonly -a webroot --webroot-path=/var/www/qgis-beta -d ${DOMAIN_TLD_BETA} 
 EOF
 
 
-docker exec -I ${QGIS_BASE} bash <<EOF
-  supervisorctl restart nginx
+docker exec -i ${QGIS_BASE} bash <<EOF
+supervisorctl restart nginx
 EOF
